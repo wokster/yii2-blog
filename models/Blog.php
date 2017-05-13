f1ae3c2ce9db67d97f5b439dbe6ac77f0a99c336
Максим Тимофеев <wokster@list.ru>
2017-05-13 15:29:25 +0300

    edit

--- models/Blog.php
+++ models/Blog.php
@@ -29,6 +29,10 @@ use common\models\ImageManager;
 class Blog extends ActiveRecord
 {
     const STATUS_LIST = ['off','on'];
+    const IMAGES_SIZE = [
+        ['50','50'],
+        ['800',null],
+    ];
     public $tags_array;
     public $file;
 
@@ -195,4 +199,26 @@ class Blog extends ActiveRecord
         }
         BlogTag::deleteAll(['tag_id'=>$arr]);
     }
+
+    public function beforeDelete()
+        {
+            if (parent::beforeDelete()) {
+                $dir = Yii::getAlias('@images').'/blog/';
+                if(file_exists($dir.$this->image)){
+                    unlink($dir.$this->image);
+                }
+                foreach (self::IMAGES_SIZE as $size){
+                    $size_dir = $size[0].'x';
+                    if($size[1] !== null)
+                        $size_dir .= $size[1];
+                    if(file_exists($dir.$this->image)){
+                        unlink($dir.$size_dir.'/'.$this->image);
+                    }
+                }
+                BlogTag::deleteAll(['blog_id'=>$this->id]);
+                return true;
+            } else {
+                return false;
+            }
+        }
 }
